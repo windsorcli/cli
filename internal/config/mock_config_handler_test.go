60b6a3fe3edb7eb91d67811b343c329d1ab4640a
Ryan VanGundy <ryan.vangundy+rmvangun@gmail.com>
2024-09-15 19:28:27 -0400

    Use matrix tests

--- internal/config/mock_config_handler_test.go
+++ internal/config/mock_config_handler_test.go
@@ -5,93 +5,122 @@ import (
 	"testing"
 )
 
-func TestMockConfigHandler_LoadConfig(t *testing.T) {
-	mockErr := errors.New("mock load config error")
+func TestMockConfigHandler(t *testing.T) {
+	mockLoadErr := errors.New("mock load config error")
+	mockGetErr := errors.New("mock get config value error")
+	mockSetErr := errors.New("mock set config value error")
+	mockSaveErr := errors.New("mock save config error")
+
 	handler := &MockConfigHandler{
 		LoadConfigFunc: func(path string) error {
-			return mockErr
+			return mockLoadErr
 		},
-	}
-
-	err := handler.LoadConfig("some/path")
-	if err != mockErr {
-		t.Errorf("LoadConfig() error = %v, wantErr %v", err, mockErr)
-	}
-}
-
-func TestMockConfigHandler_GetConfigValue(t *testing.T) {
-	mockErr := errors.New("mock get config value error")
-	handler := &MockConfigHandler{
 		GetConfigValueFunc: func(key string) (string, error) {
-			return "", mockErr
+			return "", mockGetErr
 		},
-	}
-
-	_, err := handler.GetConfigValue("someKey")
-	if err != mockErr {
-		t.Errorf("GetConfigValue() error = %v, wantErr %v", err, mockErr)
-	}
-}
-
-func TestMockConfigHandler_SetConfigValue(t *testing.T) {
-	mockErr := errors.New("mock set config value error")
-	handler := &MockConfigHandler{
 		SetConfigValueFunc: func(key, value string) error {
-			return mockErr
+			return mockSetErr
 		},
-	}
-
-	err := handler.SetConfigValue("someKey", "someValue")
-	if err != mockErr {
-		t.Errorf("SetConfigValue() error = %v, wantErr %v", err, mockErr)
-	}
-}
-
-func TestMockConfigHandler_SaveConfig(t *testing.T) {
-	mockErr := errors.New("mock save config error")
-	handler := &MockConfigHandler{
 		SaveConfigFunc: func(path string) error {
-			return mockErr
+			return mockSaveErr
 		},
 	}
 
-	err := handler.SaveConfig("some/path")
-	if err != mockErr {
-		t.Errorf("SaveConfig() error = %v, wantErr %v", err, mockErr)
+	tests := []struct {
+		name     string
+		testFunc func() error
+		wantErr  error
+	}{
+		{
+			name: "LoadConfig",
+			testFunc: func() error {
+				return handler.LoadConfig("some/path")
+			},
+			wantErr: mockLoadErr,
+		},
+		{
+			name: "GetConfigValue",
+			testFunc: func() error {
+				_, err := handler.GetConfigValue("someKey")
+				return err
+			},
+			wantErr: mockGetErr,
+		},
+		{
+			name: "SetConfigValue",
+			testFunc: func() error {
+				return handler.SetConfigValue("someKey", "someValue")
+			},
+			wantErr: mockSetErr,
+		},
+		{
+			name: "SaveConfig",
+			testFunc: func() error {
+				return handler.SaveConfig("some/path")
+			},
+			wantErr: mockSaveErr,
+		},
 	}
-}
 
-func TestMockConfigHandler_LoadConfig_Default(t *testing.T) {
-	handler := &MockConfigHandler{}
-	err := handler.LoadConfig("some/path")
-	if err != nil {
-		t.Errorf("Expected nil, got %v", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.testFunc(); err != tt.wantErr {
+				t.Errorf("%s() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			}
+		})
 	}
-}
 
-func TestMockConfigHandler_GetConfigValue_Default(t *testing.T) {
-	handler := &MockConfigHandler{}
-	value, err := handler.GetConfigValue("someKey")
-	if err != nil {
-		t.Errorf("Expected nil error, got %v", err)
-	}
-	if value != "" {
-		t.Errorf("Expected empty string, got %v", value)
-	}
-}
+	defaultHandler := &MockConfigHandler{}
 
-func TestMockConfigHandler_SetConfigValue_Default(t *testing.T) {
-	handler := &MockConfigHandler{}
-	err := handler.SetConfigValue("someKey", "someValue")
-	if err != nil {
-		t.Errorf("Expected nil, got %v", err)
+	defaultTests := []struct {
+		name      string
+		testFunc  func() (string, error)
+		wantValue string
+		wantErr   error
+	}{
+		{
+			name: "LoadConfig_Default",
+			testFunc: func() (string, error) {
+				return "", defaultHandler.LoadConfig("some/path")
+			},
+			wantValue: "",
+			wantErr:   nil,
+		},
+		{
+			name: "GetConfigValue_Default",
+			testFunc: func() (string, error) {
+				return defaultHandler.GetConfigValue("someKey")
+			},
+			wantValue: "",
+			wantErr:   nil,
+		},
+		{
+			name: "SetConfigValue_Default",
+			testFunc: func() (string, error) {
+				return "", defaultHandler.SetConfigValue("someKey", "someValue")
+			},
+			wantValue: "",
+			wantErr:   nil,
+		},
+		{
+			name: "SaveConfig_Default",
+			testFunc: func() (string, error) {
+				return "", defaultHandler.SaveConfig("some/path")
+			},
+			wantValue: "",
+			wantErr:   nil,
+		},
 	}
-}
 
-func TestMockConfigHandler_SaveConfig_Default(t *testing.T) {
-	handler := &MockConfigHandler{}
-	err := handler.SaveConfig("some/path")
-	if err != nil {
-		t.Errorf("Expected nil, got %v", err)
+	for _, tt := range defaultTests {
+		t.Run(tt.name, func(t *testing.T) {
+			value, err := tt.testFunc()
+			if err != tt.wantErr {
+				t.Errorf("%s() error = %v, wantErr %v", tt.name, err, tt.wantErr)
+			}
+			if value != tt.wantValue {
+				t.Errorf("%s() value = %v, wantValue %v", tt.name, value, tt.wantValue)
+			}
+		})
 	}
 }
