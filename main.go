af348d8b58157fccfb47c766e6a6626773cf71b7
Ryan VanGundy <ryan.vangundy+rmvangun@gmail.com>
2024-09-16 22:30:00 -0400

    Cleanup

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/windsor-hotel/cli/cmd"
 	"github.com/windsor-hotel/cli/internal/config"
 	"github.com/windsor-hotel/cli/internal/di"
-	"github.com/windsor-hotel/cli/internal/helpers" // Added import
+	"github.com/windsor-hotel/cli/internal/helpers"
 )
 
 func main() {
@@ -13,12 +13,16 @@ func main() {
 
 	// Register dependencies
 	container.Register("configHandler", &config.ViperConfigHandler{})
-	container.Register("baseHelper", func(c *di.Container) interface{} { // Pass by reference
-		configHandler, err := c.Resolve("configHandler")
+	container.Register("baseHelper", func(c *di.Container) interface{} {
+		instance, err := c.Resolve("configHandler")
 		if err != nil {
-			panic(err) // Handle error appropriately
+			panic(err)
 		}
-		return helpers.NewBaseHelper(configHandler.(config.ConfigHandler)) // Type assertion
+		configHandler, ok := instance.(config.ConfigHandler)
+		if !ok {
+			panic("resolved instance is not of type config.ConfigHandler")
+		}
+		return helpers.NewBaseHelper(configHandler)
 	})
 
 	// Inject the DI container into the cmd package
