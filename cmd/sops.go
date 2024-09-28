09656835f01ea86c57aa6b3ffa7569faf1067d40
Todd VanGundy <todd@windsor-hotel.io>
2024-09-28 09:51:52 -0400

    Added encrypt command

--- cmd/sops.go
+++ cmd/sops.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
 	"github.com/getsops/sops/v3/decrypt"
 	"github.com/spf13/cobra"
@@ -15,27 +16,66 @@ var sopsDecryptCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		file := args[0]
-		data, error := decryptFile(file)
+		data, err := decryptFile(file)
+		if err != nil {
+			return err
+		}
 
-		fmt.Print(data)
-		return error
+		fmt.Print(string(data))
+		return nil
 	},
 }
 
-// decryptFile decrypts a file using the go.mozilla.org/sops/decrypt package
+var sopsEncryptCmd = &cobra.Command{
+	Use:   "encrypt <file>",
+	Short: "Encrypt a file using SOPS",
+	Long:  `Encrypt a file to YAML format using SOPS.`,
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		file := args[0]
+		err := encryptFile(file)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("File successfully encrypted: %s\n", file)
+		return nil
+	},
+}
+
+// decryptFile decrypts a file using the SOPS package
 func decryptFile(filePath string) ([]byte, error) {
 	// Check if the file exists
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		return nil, fmt.Errorf("file does not exist: %s", filePath)
 	}
 
-	// Decrypt the file using SOPS and return the decrypted content as a byte array
+	// Decrypt the file using SOPS
 	plaintextBytes, err := decrypt.File(filePath, "yaml")
 	if err != nil {
 		return nil, fmt.Errorf("failed to decrypt file: %w", err)
 	}
 
 	return plaintextBytes, nil
+}
+
+// encryptFile encrypts a file using the SOPS CLI
+func encryptFile(filePath string) error {
+	// Check if the file exists
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return fmt.Errorf("file does not exist: %s", filePath)
+	}
+
+	// Prepare the SOPS CLI command for encryption
+	cmd := exec.Command("sops", "--encrypt", "--in-place", filePath)
+
+	// Run the command and capture output
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("failed to encrypt file: %s, %s", err, string(output))
+	}
+
+	return nil
 }
 
 func init() {
@@ -47,4 +87,5 @@ func init() {
 
 	sopsCmd := rootCmd.Commands()[len(rootCmd.Commands())-1]
 	sopsCmd.AddCommand(sopsDecryptCmd)
+	sopsCmd.AddCommand(sopsEncryptCmd)
 }
