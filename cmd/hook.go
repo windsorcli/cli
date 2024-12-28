04c30a028c6d4e58eff31b73bb784d40e25d0c60
Todd VanGundy <todd@windsor-hotel.io>
2024-12-28 11:51:31 -0500

    added windsor hook capabilities

--- cmd/hook.go
+++ cmd/hook.go
@@ -2,68 +2,79 @@ package cmd
 
 import (
 	"fmt"
+	"html/template"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+// HookContext are the variables available during hook template evaluation
+type HookContext struct {
+	// SelfPath is the unescaped absolute path to windsor
+	SelfPath string
+}
+
+// /usr/local/bin/windsor env
+
 var shellHooks = map[string][]string{
 	"zsh": {
 		`
-		_direnv_hook() {
-			trap -- '' SIGINT
-			eval "$("{{.SelfPath}}" export zsh)"
-			trap - SIGINT
-		}
-		typeset -ag precmd_functions
-		if (( ! ${precmd_functions[(I)_direnv_hook]} )); then
-			precmd_functions=(_direnv_hook $precmd_functions)
-		fi
-		typeset -ag chpwd_functions
-		if (( ! ${chpwd_functions[(I)_direnv_hook]} )); then
-			chpwd_functions=(_direnv_hook $chpwd_functions)
-		fi
+		_windsor_hook() {
+			trap -- '' SIGINT;
+			eval "$(windsor env)";
+			trap - SIGINT;
+		};
+		typeset -ag precmd_functions;
+		if (( ! ${precmd_functions[(I)_windsor_hook]} )); then
+			precmd_functions=(_windsor_hook $precmd_functions)
+		fi;
+		typeset -ag chpwd_functions;
+		if (( ! ${chpwd_functions[(I)_windsor_hook]} )); then
+			chpwd_functions=(_windsor_hook $chpwd_functions)
+		fi;
 		`,
 	},
 	"bash": {
 		`
-		_direnv_hook() {
+		_windsor_hook() {
 			local previous_exit_status=$?;
 			trap -- '' SIGINT;
-			eval "$("{{.SelfPath}}" export bash)";
+			eval "$(windsor env)";
 			trap - SIGINT;
 			return $previous_exit_status;
 		};
-		if [[ ";${PROMPT_COMMAND[*]:-};" != *";_direnv_hook;"* ]]; then
+		if [[ ";${PROMPT_COMMAND[*]:-};" != *";_windsor_hook;"* ]]; then
 			if [[ "$(declare -p PROMPT_COMMAND 2>&1)" == "declare -a"* ]]; then
-				PROMPT_COMMAND=(_direnv_hook "${PROMPT_COMMAND[@]}")
+				PROMPT_COMMAND=(_windsor_hook "${PROMPT_COMMAND[@]}")
 			else
-				PROMPT_COMMAND="_direnv_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+				PROMPT_COMMAND="_windsor_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
 			fi
 		fi
 		`,
 	},
 	"fish": {
 		`
-		function _direnv_hook --on-event fish_prompt
-			eval ("{{.SelfPath}}" export fish | source)
+		function _windsor_hook --on-event fish_prompt
+			eval (eval "$(windsor env)"; export fish | source)
 		end
 		`,
 	},
 	"tcsh": {
 		`
-		alias precmd '_direnv_hook'
-		alias _direnv_hook 'eval "{{.SelfPath}}" export tcsh"
+		alias precmd '_windsor_hook';
+		alias _windsor_hook 'eval eval "$(windsor env)"; export tcsh"
 		`,
 	},
 	"elvish": {
 		`
-		eval ("{{.SelfPath}}" export elvish | slurp)
+		eval (eval "$(windsor env)"; export elvish | slurp)
 		`,
 	},
 	"powershell": {
 		`
 		$DirenvPrompt = {
-			Invoke-Expression (& "{{.SelfPath}}" export powershell)
+			Invoke-Expression (& eval "$(windsor env)"; export powershell)
 		}
 		If ($global:Prompt -is [scriptblock]) {
 			$oldPrompt = $global:Prompt
@@ -80,16 +91,11 @@ var shellHooks = map[string][]string{
 
 var hookCmd = &cobra.Command{
 	Use:          "hook",
-	Short:        "Prints out windsor setup command for each platform.",
-	Long:         "Prints out windsor setup command for each platform.",
+	Short:        "Prints out shell hook information per platform.",
+	Long:         "Prints out shell hook information for each platform (zsh,bash,fish,tcsh, elvish,powershell).",
 	SilenceUsage: true,
 	PreRunE:      preRunEInitializeCommonComponents,
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		// Check if the correct number of arguments is provided
-		if len(args) != 1 {
-			return fmt.Errorf("Please provide exactly one argument specifying the shell name")
-		}
 
 		// Get the shell name from the arguments
 		shellName := args[0]
@@ -100,10 +106,26 @@ var hookCmd = &cobra.Command{
 			return fmt.Errorf("Unsupported shell: %s", shellName)
 		}
 
-		// Print the hook command
-		fmt.Println(hookCommand[0])
-		return nil
+		selfPath, err := os.Executable()
+		if err != nil {
+			return err
+		}
 
+		// Convert Windows path if needed
+		selfPath = strings.Replace(selfPath, "\\", "/", -1)
+		ctx := HookContext{selfPath}
+
+		hookTemplate, err := template.New("hook").Parse(hookCommand[0])
+		if err != nil {
+			return err
+		}
+
+		err = hookTemplate.Execute(os.Stdout, ctx)
+		if err != nil {
+			return err
+		}
+
+		return nil
 	},
 }
 
