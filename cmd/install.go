0fd154c8f696f26af9bd39690e83ea3171299b27
Ryan VanGundy <ryan.vangundy+rmvangun@gmail.com>
2025-01-04 10:01:29 -0500

    Fix install bug

--- cmd/install.go
+++ cmd/install.go
@@ -7,12 +7,18 @@ import (
 )
 
 var installCmd = &cobra.Command{
-	Use:   "install",
-	Short: "Install the blueprint's cluster-level services",
+	Use:     "install",
+	Short:   "Install the blueprint's cluster-level services",
+	PreRunE: preRunEInitializeCommonComponents,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// Create project components
 		if err := controller.CreateProjectComponents(); err != nil {
 			return fmt.Errorf("Error creating project components: %w", err)
+		}
+
+		// Initialize all components
+		if err := controller.InitializeComponents(); err != nil {
+			return fmt.Errorf("Error initializing components: %w", err)
 		}
 
 		// Resolve the blueprint handler
