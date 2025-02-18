0ef45ecffa8490c0edb660438cf40c3a87ffe74f
Ryan VanGundy <ryan.vangundy+rmvangun@gmail.com>
2025-02-17 23:34:13 -0500

    Fix context command

--- cmd/context.go
+++ cmd/context.go
@@ -16,21 +16,13 @@ var getContextCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		controller := cmd.Context().Value(controllerKey).(ctrl.Controller)
 
-		// New snippet: Ensure projectName is set
-		configHandler := controller.ResolveConfigHandler()
-		projectName := configHandler.GetString("projectName")
-		if projectName == "" {
-			fmt.Println("Cannot manage contexts. Please run `windsor init` to set up your project first.")
-			return nil
-		}
-
 		// Initialize components
 		if err := controller.InitializeComponents(); err != nil {
 			return fmt.Errorf("Error initializing components: %w", err)
 		}
 
 		// Resolve config handler
-		configHandler = controller.ResolveConfigHandler()
+		configHandler := controller.ResolveConfigHandler()
 
 		// Get the current context
 		currentContext := configHandler.GetContext()
@@ -50,21 +42,13 @@ var setContextCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		controller := cmd.Context().Value(controllerKey).(ctrl.Controller)
 
-		// New snippet: Ensure projectName is set
-		configHandler := controller.ResolveConfigHandler()
-		projectName := configHandler.GetString("projectName")
-		if projectName == "" {
-			fmt.Println("Cannot manage contexts. Please run `windsor init` to set up your project first.")
-			return nil
-		}
-
 		// Initialize components
 		if err := controller.InitializeComponents(); err != nil {
 			return fmt.Errorf("Error initializing components: %w", err)
 		}
 
 		// Resolve config handler
-		configHandler = controller.ResolveConfigHandler()
+		configHandler := controller.ResolveConfigHandler()
 
 		// Set the context
 		contextName := args[0]
