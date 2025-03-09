4b495a328a26f9e24506fe8e78d49f7fb80ee45e
Ryan VanGundy <ryan.vangundy+rmvangun@gmail.com>
2025-03-09 17:45:32 -0400

    correct image

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -86,6 +86,5 @@ const (
 
 const (
 	// renovate: datasource=docker depName=ghcr.io/windsorcli/windsorcli
-	// DEFAULT_WINDSOR_IMAGE = "ghcr.io/windsorcli/windsorcli:latest"
-	DEFAULT_WINDSOR_IMAGE = "windsorcli:latest"
+	DEFAULT_WINDSOR_IMAGE = "ghcr.io/windsorcli/windsorcli:latest"
 )
