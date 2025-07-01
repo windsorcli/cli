b4cb8038799d91ae12d12873fdd4db544c8e03a5
Ryan VanGundy <ryan.vangundy+rmvangun@gmail.com>
2025-07-01 17:48:41 -0400

    Add bundler

--- pkg/bundler/shims.go
+++ pkg/bundler/shims.go
@@ -5,6 +5,7 @@ import (
 	"compress/gzip"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/goccy/go-yaml"
 )
@@ -29,9 +30,12 @@ type TarWriter interface {
 type Shims struct {
 	Stat          func(name string) (os.FileInfo, error)
 	Create        func(name string) (io.WriteCloser, error)
+	ReadFile      func(name string) ([]byte, error)
+	Walk          func(root string, walkFn filepath.WalkFunc) error
 	NewGzipWriter func(w io.Writer) *gzip.Writer
 	NewTarWriter  func(w io.Writer) TarWriter
 	YamlUnmarshal func(data []byte, v any) error
+	FilepathRel   func(basepath, targpath string) (string, error)
 	YamlMarshal   func(data any) ([]byte, error)
 }
 
@@ -45,9 +49,12 @@ func NewShims() *Shims {
 		Stat: os.Stat,
 		// #nosec G304 - User-controlled output path is intentional for build artifact creation
 		Create:        func(name string) (io.WriteCloser, error) { return os.Create(name) },
+		ReadFile:      os.ReadFile,
+		Walk:          filepath.Walk,
 		NewGzipWriter: gzip.NewWriter,
 		NewTarWriter:  func(w io.Writer) TarWriter { return tar.NewWriter(w) },
 		YamlUnmarshal: yaml.Unmarshal,
+		FilepathRel:   filepath.Rel,
 		YamlMarshal:   yaml.Marshal,
 	}
 }
