89139ed73985752fb3312255a86cb1e5e7ed2e2a
Ryan VanGundy <ryan.vangundy+rmvangun@gmail.com>
2025-07-15 10:20:56 -0400

    Use goccy/go-yaml

--- pkg/blueprint/blueprint_handler_test.go
+++ pkg/blueprint/blueprint_handler_test.go
@@ -12,6 +12,7 @@ import (
 	helmv2 "github.com/fluxcd/helm-controller/api/v2"
 	kustomizev1 "github.com/fluxcd/kustomize-controller/api/v1"
 	sourcev1 "github.com/fluxcd/source-controller/api/v1"
+	"github.com/goccy/go-yaml"
 	blueprintv1alpha1 "github.com/windsorcli/cli/api/v1alpha1"
 	"github.com/windsorcli/cli/pkg/artifact"
 	"github.com/windsorcli/cli/pkg/config"
