6414b47763ddf6e70f0a76f2240dece2219eb686
Ryan VanGundy <ryan.vangundy+rmvangun@gmail.com>
2025-07-20 01:24:43 -0400

    fix(livereload): Include kustomize/ folder and don't ignore data/
    Updated constants that enumerate default settings for git-livereload. Now we wish to only include the `kustomize/` folder.

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -6,8 +6,8 @@ import "time"
 const (
 	// renovate: datasource=docker depName=ghcr.io/windsorcli/git-livereload-server
 	DEFAULT_GIT_LIVE_RELOAD_IMAGE         = "ghcr.io/windsorcli/git-livereload:v0.2.0"
-	DEFAULT_GIT_LIVE_RELOAD_RSYNC_INCLUDE = ""
-	DEFAULT_GIT_LIVE_RELOAD_RSYNC_EXCLUDE = ".windsor,.terraform,data,.volumes,.venv"
+	DEFAULT_GIT_LIVE_RELOAD_RSYNC_INCLUDE = "kustomize"
+	DEFAULT_GIT_LIVE_RELOAD_RSYNC_EXCLUDE = ".windsor,.terraform,.volumes,.venv"
 	DEFAULT_GIT_LIVE_RELOAD_RSYNC_PROTECT = "flux-system"
 	DEFAULT_GIT_LIVE_RELOAD_USERNAME      = "local"
 	DEFAULT_GIT_LIVE_RELOAD_PASSWORD      = "local"
