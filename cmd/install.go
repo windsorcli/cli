4e6c7ac8e8d6ebd9bf1950c0c5f36beadfde56a7
Ryan VanGundy <85766511+rmvangun@users.noreply.github.com>
2025-11-13 20:39:05 -0500

    refactor(cmd): Remove runtime and pipeline from install command
    Removed the deprecated runtime and pipeline constructs in favor of the new top level components.
    
    Signed-off-by: Ryan VanGundy <85766511+rmvangun@users.noreply.github.com>

--- cmd/install.go
+++ cmd/install.go
@@ -1,13 +1,11 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/spf13/cobra"
 	"github.com/windsorcli/cli/pkg/di"
-	"github.com/windsorcli/cli/pkg/pipelines"
-	"github.com/windsorcli/cli/pkg/runtime"
+	"github.com/windsorcli/cli/pkg/project"
 )
 
 var installWaitFlag bool
@@ -17,42 +15,37 @@ var installCmd = &cobra.Command{
 	Short:        "Install the blueprint's cluster-level services",
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// Get shared dependency injector from context
 		injector := cmd.Context().Value(injectorKey).(di.Injector)
 
-		// First, set up environment variables using runtime
-		deps := &runtime.Dependencies{
-			Injector: injector,
-		}
-		if err := runtime.NewRuntime(deps).
-			LoadShell().
-			CheckTrustedDirectory().
-			LoadConfig().
-			LoadSecretsProviders().
-			LoadEnvVars(runtime.EnvVarsOptions{
-				Decrypt: true,
-				Verbose: verbose,
-			}).
-			ExecutePostEnvHook(verbose).
-			Do(); err != nil {
-			return fmt.Errorf("failed to set up environment: %w", err)
-		}
-
-		// Then, run the install pipeline for blueprint installation
-		installPipeline, err := pipelines.WithPipeline(injector, cmd.Context(), "installPipeline")
+		proj, err := project.NewProject(injector, "")
 		if err != nil {
-			return fmt.Errorf("failed to set up install pipeline: %w", err)
+			return err
 		}
 
-		// Create execution context with flags
-		ctx := cmd.Context()
+		if err := proj.Context.Shell.CheckTrustedDirectory(); err != nil {
+			return fmt.Errorf("not in a trusted directory. If you are in a Windsor project, run 'windsor init' to approve")
+		}
+
+		if err := proj.Configure(nil); err != nil {
+			return err
+		}
+
+		if err := proj.Initialize(false); err != nil {
+			if !verbose {
+				return nil
+			}
+			return err
+		}
+
+		blueprint := proj.Composer.BlueprintHandler.Generate()
+		if err := proj.Provisioner.Install(blueprint); err != nil {
+			return fmt.Errorf("error installing blueprint: %w", err)
+		}
+
 		if installWaitFlag {
-			ctx = context.WithValue(ctx, "wait", true)
-		}
-
-		// Execute the install pipeline
-		if err := installPipeline.Execute(ctx); err != nil {
-			return fmt.Errorf("Error executing install pipeline: %w", err)
+			if err := proj.Provisioner.Wait(blueprint); err != nil {
+				return fmt.Errorf("error waiting for kustomizations: %w", err)
+			}
 		}
 
 		return nil
