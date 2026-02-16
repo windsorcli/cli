ad5a4295c0d6b2eab5b12c470e2f86d0e58cb683
Ryan VanGundy <85766511+rmvangun@users.noreply.github.com>
2026-02-16 12:19:03 -0500

    dont swallow error
    Signed-off-by: Ryan VanGundy <85766511+rmvangun@users.noreply.github.com>

--- cmd/init.go
+++ cmd/init.go
@@ -173,6 +173,8 @@ var initCmd = &cobra.Command{
 		} else if contextName == "local" {
 			if _, err := os.Stat(rt.TemplateRoot); os.IsNotExist(err) {
 				blueprintURL = []string{constants.GetEffectiveBlueprintURL()}
+			} else if err != nil {
+				return fmt.Errorf("error checking template root: %w", err)
 			}
 		}
 		if err := proj.Initialize(initReset, blueprintURL...); err != nil {
